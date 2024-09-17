afe0264a2995a4e6e452119fa62e465429a7bbe6
@hexlet/cli <support@hexlet.io>
2024-09-17 20:59:46 +0200

    submit rails-real-ru/nested-forms

--- rails-real-ru/nested-forms/app/models/resume.rb
+++ rails-real-ru/nested-forms/app/models/resume.rb
@@ -5,10 +5,15 @@ class Resume < ApplicationRecord
   validates :contact, presence: true
 
   # BEGIN
-  has_many :educations, dependent: :destroy, class_name: 'ResumeEducation'
-  has_many :works, dependent: :destroy, class_name: 'ResumeWork'
+  has_many :educations, class_name: 'ResumeEducation',
+                        inverse_of: :resume,
+                        dependent: :destroy
 
-  accepts_nested_attributes_for :educations, allow_destroy: true
-  accepts_nested_attributes_for :works, allow_destroy: true
+  has_many :works, class_name: 'ResumeWork',
+                   inverse_of: :resume,
+                   dependent: :destroy
+
+  accepts_nested_attributes_for :educations, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :works, reject_if: :all_blank, allow_destroy: true
   # END
 end
