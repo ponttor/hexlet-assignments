be170b8e53e49b1a03a14358719d634456c9482e
@hexlet/cli <support@hexlet.io>
2024-11-23 16:58:39 +0100

    submit rails-basics-ru/crud

--- rails-basics-ru/crud/app/models/task.rb
+++ rails-basics-ru/crud/app/models/task.rb
@@ -1,9 +1,8 @@
+# frozen_string_literal: true
+
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :status, presence: true
   validates :creator, presence: true
   validates :completed, inclusion: { in: [true, false] }
-
-  validates :name, length: { maximum: 255 }
-  validates :description, length: { maximum: 500 }, allow_blank: true
 end
